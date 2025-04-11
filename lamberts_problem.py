corrected bugs and added periapsis rotation option

lamberts_problem.py
@@ -36,6 +36,7 @@ class LambertAlgorithm:
     delta_time: float
     mu: float
     prograde: bool = True
+    z_guess: float = 0.0
 
     def __post_init__(self):
         if isinstance(self.r1_vec, list):
@@ -116,17 +117,11 @@ class LambertAlgorithm:
         return first_summand + second_summand
 
     def solve_z(self, z0):
+        print("Iterative solver for z")
         zsol = fsolve(self.F_fun, z0, fprime=self.F_fun_prime)
         self.z_solved = zsol[0]  # save the solution for later use in other methods
-        print(self.z_solved)
+        print("Iterative solver finished")
         return self.z_solved
-
-    def plot_F(self):
-        zS = np.linspace(-5, 5, num=50)
-        F_values = [self.F_fun(z) for z in zS]
-
-        plt.plot(zS, F_values)
-        plt.show()
 
     # LAGRANGE COEFFICIENTS
     @property
@@ -150,7 +145,7 @@ class LambertAlgorithm:
         return 1 / self.g_zsolved * (self.gdot_zsolved * self.r2_vec - self.r1_vec)
 
     def solve_it(self):
-        z0 = 0
+        z0 = self.z_guess
         self.solve_z(z0)
         return self.v1_vec, self.v2_vec
 

orbit_elements_and_state_vector.py
@@ -43,85 +43,21 @@ def compute_elements(r_vec, v_vec, mu):
     h = np.linalg.norm(h_vec)
     print(f"Angular momentum h is {h:.3e} km^2/s")
 
-    i = np.arccos(h_vec[2] / h)
-    print(f"Orbit inclination is {i:.3f} rad")
-    print(f"Orbit inclination is {i/to_rad:.3f} degrees")
-
-    if i > np.pi / 2:
-        print(f"Since i greater than 90 degrees, this is a retrograde orbit.")
-        print(f"Rotation of object is contrary to rotation of the body")
-    else:
-        print(f"Since i smaller than 90 degrees, this is a normal orbit.")
-        print(f"Rotation of object is same to rotation of the body.")
-
-    N_vec = np.cross([0, 0, 1], h_vec)
-    print(N_vec)
-
-    N = np.linalg.norm(N_vec)
-    print(f"{N:.3e}")
-
-    Omega = np.arccos(N_vec[0] / N)
-    # Omega is returned from [0, pi].
-    # However, we should check the quadrant based on N_Y
-    if N_vec[1] < 0:
-        print("y component of N is negative, therefore, we need to do 360-Omega")
-        Omega = 2 * np.pi - Omega
-    print(f"Omega is {Omega:.3f} rad")
-    print(f"Omega is {Omega/to_rad:.3f} degrees")
-
     e_vec = 1 / mu * ((v**2 - mu / r) * r_vec - r * v_r * v_vec)
     print(e_vec)
     e = np.linalg.norm(e_vec)
-    print(f"The eccentricity of the orbit is: {e:.3f}")
-    print_type_orbit(e)
 
-    # perigee argument
-    w = np.arccos(np.dot(N_vec, e_vec) / N / e)
-    if e_vec[2] < 0:
-        print("Solved ambiguity for periapsis")
-        w = 2 * np.pi - w
-    if i == 0:
-        w = arctan2(e_vec[1], e_vec[0])
-    print(f"The periapsis argument is {w:.3f} rad")
-    print(f"The periapsis argument is {w / to_rad:.3f} degrees")
+    w = arctan2(e_vec[1], e_vec[0])
 
     # true anomaly
     theta = np.arccos(np.dot(e_vec, r_vec) / e / r)
     if v_r < 0:
         print("Solved ambiguity for true anomaly")
         theta = 2 * np.pi - theta
-    print(f"The true anomaly is {theta:.3f} rad")
-    print(f"The true anomaly is {theta / to_rad:.3f} degrees")
 
     a = h**2 / mu / (1 - e**2)
 
-    return h, e, i, Omega, w, theta, mu, a
-
-
-def compute_state_vector(h, e, i, Omega, w, theta, mu, a):
-
-    r = h**2 / mu / (1 + e * cos(theta))
-    print(f"Distance is {r}km")
-
-    # We first get the perifocal coordinates
-    r_pf = np.array([r * cos(theta), r * sin(theta), 0])
-    v_pf = np.array([-mu / h * sin(theta), mu / h * (e + cos(theta)), 0])
-
-    print("The perifocal coordinates are: ")
-    print(f"{r_pf} km")
-    print(f"{v_pf} km/s")
-
-    R_Omega = rotz(-Omega)
-    R_incl = rotx(-i)
-    R_omega = rotz(-w)
-
-    r_vec = np.dot(np.dot(np.dot(R_Omega, R_incl), R_omega), r_pf)
-    v_vec = np.dot(np.dot(np.dot(R_Omega, R_incl), R_omega), v_pf)
-
-    print("The geocentric coordinates are: ")
-    print(f"{r_vec} km")
-    print(f"{v_vec} km/s")
-    return r_vec, v_vec, mu
+    return h, e, w, theta, a
 
 
 if __name__ == "__main__":

st_orbits_lambert.py
@@ -1,16 +1,16 @@
 import numpy as np
 import plotly.graph_objects as go
 import streamlit as st
-from orbit_elements_and_state_vector import compute_elements
 
 from lamberts_problem import LambertAlgorithm
+from orbit_elements_and_state_vector import compute_elements
 
 np.set_printoptions(precision=4)
 
 DICT_BODIES = {
-    "Earth": {"mu": 398600.4418, "radius": 6371.0},
+    "Earth": {"mu": 398600.4418, "radius": 6371.0, "z_guess": 0.0},
     "Mars": {"mu": 42828.3, "radius": 3389.5},
-    "Sun": {"mu": 132712440041.939, "radius": 695700.0},
+    "Sun": {"mu": 132712440041.939, "radius": 695700.0, "z_guess": 20},
 }
 
 ORBIT_1_COLOR = "blue"
@@ -18,6 +18,14 @@ ORBIT_2_COLOR = "green"
 ORBIT_TR_COLOR = "purple"
 DELTA_V_COLOR = "pink"
 ARROWSIZE = 1
+
+
+def perifocal_to_eci_equatorial(v_pqw, omega_deg):
+    omega = np.radians(omega_deg)
+    R_z = np.array(
+        [[np.cos(omega), -np.sin(omega), 0], [np.sin(omega), np.cos(omega), 0], [0, 0, 1]]
+    )
+    return R_z @ v_pqw
 
 
 # Orbital equation in polar coordinates
@@ -43,12 +51,14 @@ def compute_position_at_true_anomaly(a, e, omega_deg, nu_deg):
     return x, y
 
 
-def compute_velocity_at_true_anomaly(a, e, nu_deg, mu):
+def compute_velocity_at_true_anomaly(a, e, nu_deg, mu, omega_deg):
     h = np.sqrt(mu * a * (1 - e**2))
     nu = np.radians(nu_deg)
     vx = -mu / h * np.sin(nu)
     vy = mu / h * (e + np.cos(nu))
-    return np.array([vx, vy, 0])
+    v = np.array([vx, vy, 0])
+    v = perifocal_to_eci_equatorial(v, omega_deg)
+    return v
 
 
 # Streamlit app
@@ -110,14 +120,14 @@ st.text(f"r1 is {r1_vec} km")
 st.text(f"r2 is {r2_vec} km")
 
 prograde = True
-
-lambert_problem = LambertAlgorithm(r1_vec, r2_vec, delta_time, mu, prograde)
+zguess = DICT_BODIES[planet_selected]["z_guess"]
+lambert_problem = LambertAlgorithm(r1_vec, r2_vec, delta_time, mu, prograde, zguess)
 v1, v2 = lambert_problem.solve_it()
 st.text(f"Vtr 1 is {v1} km/s")
 st.text(f"Vtr 2 is {v2} km/s")
 
 # compute the transfer orbit
-_, e_tr, _, _, w_tr, _, _, a_tr = compute_elements(r1_vec, v1, mu)
+_, e_tr, w_tr, _, a_tr = compute_elements(r1_vec, v1, mu)
 x_tr, y_tr = compute_orbit(a_tr, e_tr, w_tr * 180 / np.pi)
 
 # Create Plotly figure
@@ -176,17 +186,15 @@ fig.add_trace(
 
 
 # Compute velocity vector components at current position
-v_current = compute_velocity_at_true_anomaly(a1, e1, nu_current, mu)
+v_current = compute_velocity_at_true_anomaly(a1, e1, nu_current, mu, omega1)
 st.text(f"	V at orbit 1 {v_current} km/s")
-v_desired = compute_velocity_at_true_anomaly(a2, e2, nu_desired, mu)
+v_desired = compute_velocity_at_true_anomaly(a2, e2, nu_desired, mu, omega2)
 st.text(f"	V at 0rbit 2 {v_desired} km/s")
 v_current_mag = np.linalg.norm(v_current)
 v_desired_mag = np.linalg.norm(v_desired)
 v1_mag = np.linalg.norm(v1)  # vmag_transfer orbit at 1
 v2_mag = np.linalg.norm(v2)  # vmag_transfer orbit at 2
-velocity_scale = (
-    planet_radius  # Scale factor to make the velocity vector visible on the plot
-)
+velocity_scale = a1 / 2  # Scale factor to make the velocity vector visible on the plot
 
 
 deltaV1 = v1 - v_current
@@ -196,6 +204,7 @@ deltaV2_mag = np.linalg.norm(deltaV2)  # vmag_transfer orbit at 2
 
 st.text(f"	ΔV1 {deltaV1} km/s")
 st.text(f"	ΔV2 {deltaV2} km/s")
+st.text(f"	z={lambert_problem.z_solved:.3f} ")
 
 # Arrow velocity vector orbit 1
 fig.add_annotation(